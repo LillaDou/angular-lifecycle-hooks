9517c739a065c30396a940978fb7e0468de9b1b5
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-09-16 16:55:35 +0200

    Inicio de proyecto + routes

---
+++ src/app/components/nav-bar/nav-bar.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './nav-bar.html',
+  styles: `
+    nav {
+      display: flex;
+      gap: 1rem;
+      justify-content: center;
+      align-items: center;
+    }
+    .active {
+      color: #341162;
+      font-weight: bold;
+    }
+  `
+})
+export class NavBar {}
