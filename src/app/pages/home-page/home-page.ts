242386a25fe52b348540671c89a41a87ba1dd15f
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-09-20 16:07:02 +0200

    Ciclo de vida - segunda parte con explicaciones

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { afterEveryRender, afterNextRender, Component, effect, OnChanges, OnInit } from '@angular/core';
 
 
 const log = ( ...messages: string[] ) => {
@@ -13,49 +13,77 @@ const log = ( ...messages: string[] ) => {
   imports: [],
   templateUrl: './home-page.html',
 })
-export class HomePage {
-
-
+export class HomePage implements OnInit, OnChanges {
 
 
   constructor() {
     log('Constructor llamado')
-  }
+  };
+
+
+  basicEffect = effect( ( onCleanup ) => {
+    log( 'effect', 'Disparar efectos secundarios');
+
+    onCleanup( () => {
+      log( 'onCleanup', 'Se ejecuta cuandop el efecto se va a destruir')
+    })
+  })
+
 
   ngOnInit() {
     log(
       'ngOnInit',
       "Runs once after Angular has initialized all the component's inputs.")
   }
+
   ngOnChanges() {
     log(
       'ngOnChanges',
       "Runs every time the component's inputs have changed.")
   }
+
   ngDoCheck() {
     log(
       'ngDoCheck',
       "Runs every time this component is checked for changes.")
   }
+
   ngAfterContentInit() {
     log(
       'ngAfterContentInit',
       "Runs once after the component's content has been initialized.")
   }
+
   ngAfterContentChecked() {
     log(
       'ngAfterContentChecked',
       "Runs every time this component content has been checked for changes")
   }
+
   ngAfterViewInit() {
     log(
       'ngAfterViewInit',
       "Runs once after the component's view has been initialized.")
   }
+
   ngAfterViewChecked() {
     log(
       'ngAfterViewChecked',
       "Runs every time the component's view has been checked for changes.")
-  }
+  };
+
+  ngOnDestroy() {
+    log(
+      'ngOnDestroy',
+      'Runs once before the component is destroyed')
+  };
+
+  afterNextRenderEffect = afterNextRender( () => {
+    log( 'afternextRender', 'Runs once the next time that all components have been rendered to the DOM')
+  });
+
+  afterEveryRenderEffect = afterEveryRender( () => {
+    log( 'afterEveryRender', 'Runs every time all components have been rendered to the DOM.')
+  })
 
 }
--- src/explicacion.ts
+++ src/explicacion.ts
@@ -1,7 +1,12 @@
 
-
-
 //? Los lifecycles son básicamente métodos/eventos que se llaman en momentos concretos
+
+// Se pueden ejecutar con el ng al principio como un método. Pero si queremos forzar o asegurarnos
+// de que se ejecute la función, o queremos dejar claro a otras personas lo que se va a 
+// inicializar cuando se crea el componente, utilizamos el 'implements OnInit' + el método definido
+// debajo
+
+
 //* CONSTRUCTOR
 //* Es llamado cuando se crea una instancia de nuestro componente. Es lo primero que se
 //* ejecutará
@@ -14,7 +19,16 @@
 
 // El ngAfterContentInit se ejecuta cuando el componente se ha inicializado
 
-
 // El ngOnChanges se dispara cada vez que los inputs de un componente cambian.
 // En este caso, los inputs no son las cajas de texto que se rellenan. Hablamos de inputs
 // cuando tenemos una señal de entrada (ejemplo: creamos una nueva propiedad name = input(...))
+
+// El ngOnDestroy es un método muy útil para hacer limpieza de timers, intervalos de tiempo, cancelar 
+// de suscripciones...
+
+// El efecto se dispara tan pronto como se ha inicializado el componente. En este caso, aparece despu'es del constructor, 
+// ngOnInit y ngDoCheck.
+// Se pueden utilizar para hacer tareas específicas. No es recomendado usarlo para peticiones http, 
+// ya que es algo muy volatil. Para ello es mejor usar el resource o el rxResource
+// Dentro del efecto tenemos el onCleaup. Se comporta muy similar al ngOnDestroy, ejecutandose cuando
+// el efecto se va a destruir.
